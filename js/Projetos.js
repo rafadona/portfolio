1a867491a8042b7c90619dd750db5da22c31f872
Rafael <rafa_dona@hotmail.com>
2021-10-15 17:34:13 -0300

    outros projetos funcionando

---
+++ js/Projetos.js
@@ -0,0 +1,74 @@
+
+const Projetos = [
+    {
+        id: 1,
+        href: "asa",
+        nome: "Ação Solidária Adventista",
+        site: "https://asa-indaiatuba-central.vercel.app",
+        github: ""
+    },
+    {
+        id: 2,
+        nome: "Loja Virtual",
+        href: "lojaVirtual",
+        site: "https://ecommerce-rafadona.vercel.app/",
+        github: "https://github.com/rafadona/ecommerce"
+    },
+    {
+        id: 3,
+        nome: "Vizualização de Dados",
+        href: "vizuDados",
+        site: "https://ecommerce-rafadona.vercel.app/",
+        github: "https://github.com/rafadona/ecommerce"
+    },
+    {
+        id: 4,
+        nome: "RAFABANK",
+        href: "rafabank",
+        site: "https://rafadona.github.io/rafabank/",
+        github: "https://github.com/rafadona/rafabank"
+    },
+    {
+        id: 5,
+        nome: "Portfólio",
+        href: "portfolio",
+        site: "https://rafadona.github.io/portfolio/",
+        github: "https://github.com/rafadona/portfolio"
+    },
+
+];
+
+const portfolio = document.querySelector(".portfolio");
+
+
+function outrosProjetos(Projetos) {
+
+    return `
+    ${Projetos.map((projeto) => {
+        return `<div>
+              <div class="container">
+                <a href="./${projeto.href}.html" class="portfolio__item">
+                  <img
+                    src="../img/portfolio-0${projeto.id}.jpg"
+                    alt=""
+                    class="portfolio__img"
+                  />
+                </a>
+              </div>
+        
+              <a class="social-list__link btn__portfolio__item" href="./${projeto.href}.html">
+                <i class="fas fa-globe-americas fa-lg">
+                  ${projeto.nome}
+                </i>
+              </a>
+            </div>
+          </div>`;
+    }).join(``)}`;
+}
+
+portfolio.insertAdjacentHTML("afterbegin", outrosProjetos(Projetos));
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -8,5 +9,8 @@ navToggle.addEventListener('click', () => {
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
-    })
-})
+    });
+});
+
+
+
